9a171ec2a093a52c6123e04038f5bbb788739a5d
Dmitriy Ozyabkin <ozyabkin.d@gmail.com>
2024-03-12 20:00:41 +0500

    almost done

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,9 +4,39 @@
 
 
 
-// int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number: ");
+int initNumber = Convert.ToInt32(Console.ReadLine());
+if (initNumber < 0)
+{
+    initNumber *= -1;
+}
 
-// while (number > 0)
+int lastDigit = initNumber % 10;
+int digit = initNumber;
+
+if (initNumber < 10)
+{
+    Console.Write(initNumber);
+}
+else
+{
+    while (initNumber > 10)
+    {
+        int rang = 1;
+        digit = initNumber;
+        while (digit > 9)
+        {
+            if (digit / 10 != 0)
+            {
+                rang *= 10;
+                digit /= 10;
+            }
+        }
+        Console.Write($"{digit}, ");
+        initNumber -= digit * rang;
+    }
+    Console.Write(lastDigit);
+}
 
 
 
@@ -14,10 +44,32 @@
 
 
 
-
-
-
-
-
-
-Console.Write(3 / 10);
+// using System;
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Введите натуральное число N: ");
+//         int N = Convert.ToInt32(Console.ReadLine());
+//         if (N < 10)
+//         {
+//             Console.WriteLine(N);
+//         }
+//         else
+//         {
+//             while (N > 0)
+//             {
+//                 int currentDigit = N % 10;
+//                 N /= 10;
+//                 if (N > 0)
+//                 {
+//                     Console.Write(currentDigit + ",");
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine(currentDigit);
+//                 }
+//             }
+//         }
+//     }
+// }
